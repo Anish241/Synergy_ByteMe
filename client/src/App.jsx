8d57a04f37eb31855ae105f4e3ecc4afabbd378d
Bodhisatya Ghosh <rommelghosh@gmail.com>
2024-02-24 12:43:40 +0530

    created admin login page level 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { Login } from './pages';
 
 function App() {
   return (
     <div>
       <h1>hello</h1>
+      <Router>
+        <Routes>    
+            <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }

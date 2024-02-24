d7a278725a7e8fbf1c901b9281be809ec103c02b
Anish <aniish9.18@gmail.com>
2024-02-24 22:43:12 +0530

    bank

--- client/src/pages/Bank/Login_Bank.jsx
+++ client/src/pages/Bank/Login_Bank.jsx
@@ -3,6 +3,7 @@ import { Navbar,Footer } from '../../components'
 import { useState,useEffect } from "react";
 import illustration from "../../assets/illustration-intro.png";
 import { AiFillPlayCircle } from "react-icons/ai";
+import { BankLogin } from '../../api/Bank';
 
 
 const Login_Bank = () => {
@@ -27,7 +28,23 @@ const Login_Bank = () => {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
 
-       
+        <button
+              type="button"
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+              onClick={
+                async () => {
+                  const res = await BankLogin();
+                  if(res){
+                    window.location.href = "/bank/dash";
+                  }
+                }
+              }
+            >
+              <AiFillPlayCircle className="text-white mr-2" />
+              <p className="text-white text-base font-semibold">
+                Bank Login
+              </p>
+            </button>
       
       </div>
       </div>

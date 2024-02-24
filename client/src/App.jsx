17995791311c633ee4383b701fc4710de540cb94
Vi-shub <Smsharma3121@gmail.com>
2024-02-24 13:24:09 +0530

    done2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import "./App.css";
-import Home from "./pages/Home";
+import { Home } from "./pages";
 
 function App() {
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import { Navbar, Footer } from "../components";
 import { Fragment } from "react";
 
 const Home = () => {

27a9f2983f2b8d1f8b559db6b3b99bc162e090c4
Vi-shub <Smsharma3121@gmail.com>
2024-02-24 13:18:33 +0530

    nav & foot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
+
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <AnimatePresence>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </AnimatePresence>
   );
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Fragment } from "react";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+          <Navbar />
+          <div className="text-white">Homewsrerdtrftyghjszxdcfvgbhnjmkdxfcgvbhnjdfcvghbnjsxdcfvgbhjnkm</div>
+          <Footer />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;

ab0abceece9889cbc4968624f5c284da6442cf94
Vi-shub <Smsharma3121@gmail.com>
2024-02-24 17:05:37 +0530

    hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,27 +1,21 @@
-import React from 'react'
-import illustration from '../assets/illustration-intro.png'
-import curv from '../assets/bg-curvy-desktop.svg'
-import curvmb from '../assets/bg-curvy-mobile.svg'
-
+import React from 'react';
+import illustration from '../assets/illustration-intro.png';
+import curv from '../assets/bg-curvy-desktop.svg';
+import curvmb from '../assets/bg-curvy-mobile.svg';
 
 const Hero = () => {
-    return (
-      <section className="p-8 relative">
+  return (
+    <section className="p-8 relative gradient-bg-services" >
       <picture className="absolute w-full bottom-0 left-0" style={{ zIndex: '-10' }}>
-        <source
-          media="(min-width: 768px)"
-          srcSet={curv}
-        />
+        <source media="(min-width: 768px)" srcSet={curv} />
         <img src={curvmb} alt="" className="w-full" />
       </picture>
 
-      
-        <img
-          src={illustration}
-          alt=""
-          className="block mx-auto mb-10 lg:mb-20"
-        />
-     
+      <img
+        src={illustration}
+        alt=""
+        className="block mx-auto mb-10 lg:mb-20"
+      />
 
       <article className="text-center max-w-3xl mx-auto">
         <h1
@@ -31,13 +25,12 @@ const Hero = () => {
         </h1>
         <p className="mb-10 text-white">
           Fylo stores all your most important files in one secure location.
-          Access them wherever you need, share and collaborate with friends
+          Access them wherever you need, share and collaborate with friends,
           family, and co-workers.
         </p>
-        <button className="btn list-item">Get Started</button>
       </article>
     </section>
-    );
-  };
-  
-  export default Hero;
+  );
+};
+
+export default Hero;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Navbar, Footer } from "../components";
 import { Fragment } from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <Fragment>
       <div className="min-h-screen">
-        
           <Navbar />
+          <Hero />
           <Footer />
-        
       </div>
     </Fragment>
   );

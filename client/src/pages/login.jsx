4bafe7a91da045f2e2461a65ce30904a2fcc84b1
Vi-shub <Smsharma3121@gmail.com>
2024-02-25 05:40:42 +0530

    login done

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,66 +1,68 @@
 import React, { useState } from "react";
-
-import {Box, Card, CardContent, CardActions, TextField, Typography, Button} from '@mui/material';
-
+import { Box, Card, CardContent, CardActions, TextField, Typography, Button } from '@mui/material';
 import { verify } from "../api/Login";
-
 import toast from "react-hot-toast";
 
-export default function Login(){
+export default function Login() {
 
-    const boxStyle = {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
-          m: 1,
-          minWidth: "20vw",
-          height: "40vh",
-        },
+  const boxStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > :not(style)': {
+      m: 1,
+      minWidth: "20vw",
+      height: "40vh",
+    },
+  }
+
+  const cardStyle = {
+    backgroundImage: '../assets/back.png', // Replace with the actual path to your image
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.15)',
+    backdropFilter: 'blur(5px)',
+  }
+
+  const [pass, setPass] = useState("default");
+
+  const handleChange = (event) => setPass(event.target.value);
+
+  console.log(pass)
+
+  const onSubmit = async () => {
+    const isVerified = await verify(pass);
+    if (isVerified) {
+      window.location.href = "/admin";
+    }
+    else {
+      toast.error("Invalid Password");
     }
 
-    const[pass,setPass] = useState("default");
+  }
 
-    const handleChange = (event) => setPass(event.target.value);
+  return (
+    <div className="centerflex coverfullscreen eth-card">
+      <Box sx={boxStyle}>
+        <Card elevation={5} className="centerflex flex-col" sx={cardStyle}>
+          <CardContent>
+            <Typography variant="h3" className="centerflex">
+              Login
+            </Typography>
+          </CardContent>
 
-    console.log(pass)
+          <CardContent className="centerflex">
+            <TextField
+              required
+              label={"Admin password"} onChange={handleChange} />
+          </CardContent>
 
-    const onSubmit = async () => {
-        const isVerified = await verify(pass);
-        if(isVerified){
-            window.location.href = "/admin";
-        }
-        else{
-            toast.error("Invalid Password");
-        }
-        
-    }
-
-    //NOTE TO ANISH : typing the paassword automatically changes it in "pass" variable defined above
-    //Clicking the button will reload the page, hence reset the password
-
-    return(
-        <div className="centerflex coverfullscreen backgroundcol-black">
-            <Box sx={boxStyle}>
-                <Card elevation={5} className="centerflex flex-col">
-                    <CardContent>
-                        <Typography variant="h3" className="centerflex">
-                        Login
-                        </Typography>
-                    </CardContent>
-
-                    <CardContent className="centerflex">
-                        <TextField
-                        required
-                        label={"Admin password"} onChange={handleChange}/>
-                    </CardContent>   
-
-                    <CardActions>
-                        <Button size="small" 
-                         onClick = {onSubmit}
-                        >Submit</Button>
-                    </CardActions> 
-                </Card>
-            </Box>
-        </div>
-    )
+          <CardActions>
+            <Button size="small"
+              onClick={onSubmit}
+            >Submit</Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </div>
+  )
 }
